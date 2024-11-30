30b1056857b81340e4777de8187aac842dedec62
Minh Dinh <55818127+superoft1@users.noreply.github.com>
2024-11-30 22:50:49 +0700

    * Update spawn zombie

--- Assets/_Game/Scripts/Zombie/ZombieController.cs
+++ Assets/_Game/Scripts/Zombie/ZombieController.cs
@@ -1,53 +1,118 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
-namespace ZombieShooter
+public class ZombieController : MonoBehaviour
 {
-    public class ZombieController : MonoBehaviour
+    public GameObject target;
+    public float walkingSpeed;
+    public float runningSpeed;
+    Animator anim;
+    NavMeshAgent agent;
+
+    enum STATE { IDLE, WANDER, ATTACK, CHASE, DEAD };
+    STATE state = STATE.IDLE;
+
+    // Start is called before the first frame update
+    void Start()
     {
-        public static readonly int HashAnimatorWalk = Animator.StringToHash("Walk");
-        public static readonly int HashAnimatorRun = Animator.StringToHash("Run");
-        public static readonly int HashAnimatorAttack = Animator.StringToHash("Attack");
-        public static readonly int HashAnimatorDeath = Animator.StringToHash("Dead");
+        anim = this.GetComponent<Animator>();
+        agent = this.GetComponent<NavMeshAgent>();
+    }
 
-        Animator anim;
+    void TurnOffTriggers()
+    {
+        anim.SetBool("Walk", false);
+        anim.SetBool("Attack", false);
+        anim.SetBool("Run", false);
+        anim.SetBool("Dead", false);
+    }
 
-        // Start is called before the first frame update
-        void Start()
+    float DistanceToPlayer()
+    {
+
+        return Vector3.Distance(target.transform.position, this.transform.position);
+    }
+
+    bool CanSeePlayer()
+    {
+        if (DistanceToPlayer() < 10)
+            return true;
+        return false;
+    }
+
+    bool ForgetPlayer()
+    {
+        if (DistanceToPlayer() > 20)
+            return true;
+        return false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null)
         {
-            anim = this.GetComponent<Animator>();
+            target = GameObject.FindWithTag("Player");
+            return;
         }
-
-        // Update is called once per frame
-        void Update()
+        switch (state)
         {
-            if (Input.GetKey(KeyCode.W))
-            {
-                anim.SetBool(HashAnimatorWalk, true);
-            }
-            else
-                anim.SetBool(HashAnimatorWalk, false);
+            case STATE.IDLE:
+                if (CanSeePlayer()) state = STATE.CHASE;
+                else if(Random.Range(0,5000) < 5)
+                    state = STATE.WANDER;
+                break;
+            case STATE.WANDER:
+                if (!agent.hasPath)
+                {
+                    float newX = this.transform.position.x + Random.Range(-5, 5);
+                    float newZ = this.transform.position.z + Random.Range(-5, 5);
+                    float newY = Terrain.activeTerrain.SampleHeight(new Vector3(newX, 0, newZ));
+                    Vector3 dest = new Vector3(newX, newY, newZ);
+                    agent.SetDestination(dest);
+                    agent.stoppingDistance = 0;
+                    TurnOffTriggers();
+                    agent.speed = walkingSpeed;
+                    anim.SetBool("Walk", true);
+                }
+                if (CanSeePlayer()) state = STATE.CHASE;
+                else if (Random.Range(0, 5000) < 5)
+                {
+                    state = STATE.IDLE;
+                    TurnOffTriggers();
+                    agent.ResetPath();
+                }
+                break;
+            case STATE.CHASE:
+                agent.SetDestination(target.transform.position);
+                agent.stoppingDistance = 5;
+                TurnOffTriggers();
+                agent.speed = runningSpeed;
+                anim.SetBool("Run", true);
 
-            if (Input.GetKey(KeyCode.R))
-            {
-                anim.SetBool(HashAnimatorRun, true);
-            }
-            else
-                anim.SetBool(HashAnimatorRun, false);
+                if (agent.remainingDistance <= agent.stoppingDistance && !agent.pathPending)
+                {
+                    state = STATE.ATTACK;
+                }
 
-            if (Input.GetKey(KeyCode.A))
-            {
-                anim.SetBool(HashAnimatorAttack, true);
-            }
-            else
-                anim.SetBool(HashAnimatorAttack, false);
+                if (ForgetPlayer())
+                {
+                    state = STATE.WANDER;
+                    agent.ResetPath();
+                }
 
-            if (Input.GetKey(KeyCode.D))
-            {
-                anim.SetBool(HashAnimatorDeath, true);
-            }
-
+                break;
+            case STATE.ATTACK:
+                TurnOffTriggers();
+                anim.SetBool("Attack", true);
+                this.transform.LookAt(target.transform.position);
+                if (DistanceToPlayer() > agent.stoppingDistance + 2)
+                    state = STATE.CHASE;
+                break;
+            case STATE.DEAD:
+                break;
         }
     }
 }
